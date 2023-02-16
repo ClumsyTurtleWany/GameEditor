6c4c6248e014a53d21e6cea8a1e0517a90e3e4fe
ClumsyTurtleWany <kdw41424142@gmail.com>
2023-02-16 19:08:48 +0900

    1. 마우스 피킹 작성 중

--- GameEditorTest_Ver0/MousePicker.cpp
+++ GameEditorTest_Ver0/MousePicker.cpp
@@ -13,5 +13,21 @@ void MousePicker::Update()
 {
 	POINT ptCursor = Input::getInstance()->m_ptPos;
 
-	Vector3 Vertex;
+	Vector3 v;
+	v.x = (((2.0f * ptCursor.x) / ClientWidth) - 1) / Projection._11;
+	v.y = -(((2.0f * ptCursor.y) / ClientHeight) - 1) / Projection._22;
+
+	Matrix inversedView = DirectX::XMMatrixInverse(NULL, View);
+
+	MouseRay.position = Vector3(0.0f, 0.0f, 0.0f);
+	MouseRay.direction = Vector3(v.x, v.y, v.z);
+	MouseRay.position = DirectX::XMVector3TransformCoord(MouseRay.position, inversedView);
+	MouseRay.direction = DirectX::XMVector3TransformNormal(MouseRay.direction, inversedView);
+	MouseRay.direction.Normalize();
+
+}
+
+bool MousePicker::CheckPick(const Vector3& v0, const Vector3& v1, const Vector3& v2)
+{
+	return false;
 }
--- GameEditorTest_Ver0/MousePicker.h
+++ GameEditorTest_Ver0/MousePicker.h
@@ -6,6 +6,11 @@ class MousePicker
 {
 public:
 	Ray MouseRay;
+	float ClientWidth;
+	float ClientHeight;
+
+	Matrix Projection;
+	Matrix View;
 
 public:
 	MousePicker();
@@ -13,6 +18,9 @@ public:
 
 public:
 	void Update();
+
+public:
+	bool CheckPick(const Vector3& v0, const Vector3& v1, const Vector3& v2);
 
 
 };
