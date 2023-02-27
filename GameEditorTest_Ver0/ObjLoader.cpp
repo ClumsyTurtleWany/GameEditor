77e75f284bd730971dafd73d34945497e6ee319a
ClumsyTurtleWany <kdw41424142@gmail.com>
2023-02-27 10:11:28 +0900

    1. Added TinyXML Open Source. 2. Added ObjLoader.

---
+++ GameEditorTest_Ver0/ObjLoader.cpp
@@ -0,0 +1,16 @@
+#include "ObjLoader.h"
+
+bool ObjLoader::LoadMtl(std::wstring filename)
+{
+	return false;
+}
+
+bool ObjLoader::LoadSmd(std::wstring filename)
+{
+	return false;
+}
+
+bool ObjLoader::LoadObj(std::wstring filename)
+{
+	return false;
+}
