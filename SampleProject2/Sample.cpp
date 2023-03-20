4922b5dfd4a29c307bab38cdce8c9656987dbd52
Lord-Jaraxxus <ym970812@naver.com>
2023-03-20 16:07:11 +0900

    실행되는 버전
    실행 되나안되나 체크햇어야하는데 ㅎㅎㅈㅅㅋㅋ

--- SampleProject2/Sample.cpp
+++ SampleProject2/Sample.cpp
@@ -1,23 +1,23 @@
 #include "Sample.h"
-#include "WindowsClient.h"
-#include "Actor.h"
-#include "PlaneComponent.h"
-#include "CameraSystem.h"
-#include "RenderSystem.h"
-#include "LightSystem.h"
-#include "DirectionalLight.h"
-#include "SkyBoxComponent.h"
+//#include "WindowsClient.h"
+//#include "Actor.h"
+//#include "PlaneComponent.h"
+//#include "CameraSystem.h"
+//#include "RenderSystem.h"
+//#include "LightSystem.h"
+//#include "DirectionalLight.h"
+//#include "SkyBoxComponent.h"
 #include "FBXLoader.hpp"
-#include "Landscape.h"
-#include "SkeletalMeshComponent.h"
-#include "MaterialManager.h"
-#include "SkyRenderSystem.h"
-#include "SkyDomeComponent.h"
-//추가
-#include "AnimationComponent.h"
-#include "UpdateAnimSystem.h"
-#include "MovementSystem.h"
-#include "Character.h"
+//#include "Landscape.h"
+//#include "SkeletalMeshComponent.h"
+//#include "MaterialManager.h"
+//#include "SkyRenderSystem.h"
+//#include "SkyDomeComponent.h"
+////추가
+//#include "AnimationComponent.h"
+//#include "UpdateAnimSystem.h"
+//#include "MovementSystem.h"
+//#include "Character.h"
 
 ///////////////////
 //Effect Include
@@ -177,218 +177,218 @@ bool SampleCore::Initialize()
 	EFFECTUTIL::SPRITE_MGR.init();
 	EFFECTUTIL::EFFECT_MGR.init();
 
-	//테스트 시스템 추가
-	//ECS::TestSystem* test = new ECS::TestSystem;
-	//MainWorld.AddSystem(test);
-	//MainWorld.RemoveSystem(test);
+	////테스트 시스템 추가
+	////ECS::TestSystem* test = new ECS::TestSystem;
+	////MainWorld.AddSystem(test);
+	////MainWorld.RemoveSystem(test);
 
-	// 1. Actor 생성
-	Actor* actor = new Actor;
+	//// 1. Actor 생성
+	//Actor* actor = new Actor;
 
-	// 2. Static Mesh Component 추가.
-	auto comp = actor->AddComponent<StaticMeshComponent>();
+	//// 2. Static Mesh Component 추가.
+	//auto comp = actor->AddComponent<StaticMeshComponent>();
 
-	// 3. Material 생성
-	Material* material = new Material;
+	//// 3. Material 생성
+	//Material* material = new Material;
 
-	// 4. 텍스쳐 로드 및 머테리얼에 추가.
-	DXTexture* texture = nullptr;
-	if (DXTextureManager::GetInstance()->Load(L"../resource/Default.bmp"))
-	{
-		texture = DXTextureManager::GetInstance()->GetTexture(L"../resource/Default.bmp");
-	}
-	if (texture != nullptr)
-	{
-		material->SetDiffuseTexture(texture);
-	}
+	//// 4. 텍스쳐 로드 및 머테리얼에 추가.
+	//DXTexture* texture = nullptr;
+	//if (DXTextureManager::GetInstance()->Load(L"../resource/Default.bmp"))
+	//{
+	//	texture = DXTextureManager::GetInstance()->GetTexture(L"../resource/Default.bmp");
+	//}
+	//if (texture != nullptr)
+	//{
+	//	material->SetDiffuseTexture(texture);
+	//}
 
-	DXTexture* normalMaptexture = nullptr;
-	if (DXTextureManager::GetInstance()->Load(L"../resource/NormalMap/normal1.bmp"))
-	{
-		normalMaptexture = DXTextureManager::GetInstance()->GetTexture(L"../resource/NormalMap/normal1.bmp");
-	}
-	if (normalMaptexture != nullptr)
-	{
-		material->SetNormalTexture(normalMaptexture);
-	}
+	//DXTexture* normalMaptexture = nullptr;
+	//if (DXTextureManager::GetInstance()->Load(L"../resource/NormalMap/normal1.bmp"))
+	//{
+	//	normalMaptexture = DXTextureManager::GetInstance()->GetTexture(L"../resource/NormalMap/normal1.bmp");
+	//}
+	//if (normalMaptexture != nullptr)
+	//{
+	//	material->SetNormalTexture(normalMaptexture);
+	//}
 
-	material->SetPixelShader(DXShaderManager::GetInstance()->GetPixelShader(L"NormalMap"));
+	//material->SetPixelShader(DXShaderManager::GetInstance()->GetPixelShader(L"NormalMap"));
 
-	// 5. 평면 메쉬 생성 후 머테리얼 세팅. 
-	PlaneComponent* plane = new PlaneComponent;
-	plane->SetMaterial(material);
-	plane->CalcTangent();
+	//// 5. 평면 메쉬 생성 후 머테리얼 세팅. 
+	//PlaneComponent* plane = new PlaneComponent;
+	//plane->SetMaterial(material);
+	//plane->CalcTangent();
 
-	// 6. 스태틱 메쉬에 평면 메쉬 추가.
-	comp->Meshes.push_back(*plane);
+	//// 6. 스태틱 메쉬에 평면 메쉬 추가.
+	//comp->Meshes.push_back(*plane);
 
-	// 7. 액터에 트랜스 폼 추가.
-	auto transformComp = actor->AddComponent<TransformComponent>();
-	transformComp->Translation = Vector3(0.0f, 0.0, 100.0f);
-	//transformComp->Rotation = Vector3(0.0f, 3.14f / 4.0f, 0.0f);
-	transformComp->Rotation = Vector3(0.0f, 0.0f, 0.0f);
-	transformComp->Scale = Vector3(20.0f, 20.0f, 1.0f);
+	//// 7. 액터에 트랜스 폼 추가.
+	//auto transformComp = actor->AddComponent<TransformComponent>();
+	//transformComp->Translation = Vector3(0.0f, 0.0, 100.0f);
+	////transformComp->Rotation = Vector3(0.0f, 3.14f / 4.0f, 0.0f);
+	//transformComp->Rotation = Vector3(0.0f, 0.0f, 0.0f);
+	//transformComp->Scale = Vector3(20.0f, 20.0f, 1.0f);
 
-	//<커스텀 이벤트 테스트>
-	//---------------------------------------------
+	////<커스텀 이벤트 테스트>
+	////---------------------------------------------
 
-	MainWorld.emit<CustomEvent>({856, FALSE});
+	//MainWorld.emit<CustomEvent>({856, FALSE});
 
-	//---------------------------------------------
-
-
-	// 8. 액터에 카메라 추가.
-	Actor* cameraActor = new Actor;
-	DebugCamera = cameraActor->AddComponent<Camera>();
-	DebugCamera->CreateViewMatrix(Vector3(0.0f, 5.0f, -100.0f), Vector3(0.0f, 0.0f, 0.0f), Vector3(0.0f, 1.0, 0.0f));
-	DebugCamera->CreateProjectionMatrix(1.0f, 10000.0f, PI * 0.25, (DXDevice::g_ViewPort.Width) / (DXDevice::g_ViewPort.Height));
-
-	// Sky Box
-	Actor* skyActor = new Actor;
-	auto skyBox = skyActor->AddComponent<SkyBoxComponent>();
-	if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/ft.bmp"))
-	{
-		skyBox->SetFrontTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/ft.bmp"));
-	}
-
-	if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/bk.bmp"))
-	{
-		skyBox->SetBackTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/bk.bmp"));
-	}
-
-	if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/lt.bmp"))
-	{
-		skyBox->SetLeftTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/lt.bmp"));
-	}
-
-	if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/rt.bmp"))
-	{
-		skyBox->SetRightTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/rt.bmp"));
-	}
-
-	if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/up.bmp"))
-	{
-		skyBox->SetUpperTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/up.bmp"));
-	}
-
-	if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/dn.bmp"))
-	{
-		skyBox->SetLowerTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/dn.bmp"));
-	}
-	float scale = 500.0f;
-	auto skyTransformComp = skyActor->AddComponent<TransformComponent>();
-	skyTransformComp->Translation = Vector3(0.0f, 0.0, 0.0f);
-	skyTransformComp->Rotation = Vector3(0.0f, 0.0f, 0.0f);
-	skyTransformComp->Scale = Vector3(scale, scale, scale);
-	
+	////---------------------------------------------
 
 
-	// Fbx Loader Test
-	Character* fbxActor = new Character;
-	auto fbxMeshComp = fbxActor->AddComponent<SkeletalMeshComponent>();
-	if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Hulk_fbx/hulk_removeTwist.FBX"))
-	{
-		//FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/charMob.FBX", fbxMeshComp);
-		FBXLoader::GetInstance()->GenerateSkeletalMeshFromFileData(L"../resource/FBX/Hulk_fbx/hulk_removeTwist.FBX", fbxMeshComp);
-	}
-	auto fbxAnimComp = fbxActor->AddComponent<AnimationComponent>();
-	if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Hulk_fbx/Mutant_Punch_Retargeted.FBX"))
-	{
-		FBXLoader::GetInstance()->GenerateAnimationFromFileData(L"../resource/FBX/Hulk_fbx/Mutant_Punch_Retargeted.FBX", fbxAnimComp);
-	}
-	auto fbxTransformComp = fbxActor->GetComponent<TransformComponent>();
-	fbxTransformComp->Scale = Vector3(10.f, 10.f, 10.f);
-	fbxTransformComp->Rotation = Vector3(0.0f, 90.0f, 0.0f);
-	fbxTransformComp->Translation = Vector3(0.0f, 0.0f, 100.0f);
+	//// 8. 액터에 카메라 추가.
+	//Actor* cameraActor = new Actor;
+	//DebugCamera = cameraActor->AddComponent<Camera>();
+	//DebugCamera->CreateViewMatrix(Vector3(0.0f, 5.0f, -100.0f), Vector3(0.0f, 0.0f, 0.0f), Vector3(0.0f, 1.0, 0.0f));
+	//DebugCamera->CreateProjectionMatrix(1.0f, 10000.0f, PI * 0.25, (DXDevice::g_ViewPort.Width) / (DXDevice::g_ViewPort.Height));
 
-	auto fbxMovementComp = fbxActor->GetComponent<MovementComponent>();
-	fbxMovementComp->Speed = 10.0f;
-	fbxMovementComp->Destination = Vector3(0.0f, 0.0f, -100.0f);
-	MainWorld.AddEntity(fbxActor);
+	//// Sky Box
+	//Actor* skyActor = new Actor;
+	//auto skyBox = skyActor->AddComponent<SkyBoxComponent>();
+	//if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/ft.bmp"))
+	//{
+	//	skyBox->SetFrontTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/ft.bmp"));
+	//}
 
-	Actor* landscapeActor = new Actor;
-	auto landscape = landscapeActor->AddComponent<Landscape>();
-	landscape->Build(8, 8, 7);
-	landscape->SetCamera(DebugCamera);
-	MainWorld.AddEntity(landscapeActor);
+	//if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/bk.bmp"))
+	//{
+	//	skyBox->SetBackTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/bk.bmp"));
+	//}
 
-	/*Actor* skySphereActor = new Actor;
-	auto skySphereComp = skySphereActor->AddComponent<StaticMeshComponent>();
-	if (FBXLoader::GetInstance()->Load(L"../resource/FBX/SkySphere.fbx"))
-	{
-		FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/SkySphere.fbx", skySphereComp);
-	}
-	MainWorld.AddEntity(skySphereActor);
-	auto skySphereTransform = skySphereActor->GetComponent<TransformComponent>();
-	skySphereTransform->Scale = Vector3(20.0f, 20.0f, 20.0f);*/
+	//if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/lt.bmp"))
+	//{
+	//	skyBox->SetLeftTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/lt.bmp"));
+	//}
 
+	//if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/rt.bmp"))
+	//{
+	//	skyBox->SetRightTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/rt.bmp"));
+	//}
 
-	/*Actor* skyDomeActor = new Actor;
-	auto skyDomeComp = skyDomeActor->AddComponent<StaticMeshComponent>();
-	if (FBXLoader::GetInstance()->Load(L"../resource/FBX/CloudDome.fbx"))
-	{
-		FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/CloudDome.fbx", skyDomeComp);
-	}
-	Material* skyDomeMaterial = new Material;
-	skyDomeMaterial->PixelShaderCodeName = L"T_UI";
-	skyDomeMaterial->DiffuseTextureName = L"../resource/FBX/CloudSky2.png";
-	MaterialManager::GetInstance()->AddMaterial(L"SkyDome", skyDomeMaterial);
-	for (auto& mesh : skyDomeComp->Meshes)
-	{
-		mesh.MaterialSlot = skyDomeMaterial;
-	}
-	MainWorld.AddEntity(skyDomeActor);
-	auto skyDomeTransform = skyDomeActor->GetComponent<TransformComponent>();
-	skyDomeTransform->Scale = Vector3(10.0f, 10.0f, 10.0f);*/
+	//if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/up.bmp"))
+	//{
+	//	skyBox->SetUpperTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/up.bmp"));
+	//}
 
-	Actor* skyDomeActor = new Actor;
-	auto skyDomeComp = skyDomeActor->AddComponent<SkyDomeComponent>(); 
-	skyDomeComp->Scale = Vector3(500.0f, 500.0f, 500.0f);
-	MainWorld.AddEntity(skyDomeActor);
-	MainWorld.AddSystem(new SkyRenderSystem);
+	//if (DXTextureManager::GetInstance()->Load(L"../resource/Sky/dn.bmp"))
+	//{
+	//	skyBox->SetLowerTexture(DXTextureManager::GetInstance()->GetTexture(L"../resource/Sky/dn.bmp"));
+	//}
+	//float scale = 500.0f;
+	//auto skyTransformComp = skyActor->AddComponent<TransformComponent>();
+	//skyTransformComp->Translation = Vector3(0.0f, 0.0, 0.0f);
+	//skyTransformComp->Rotation = Vector3(0.0f, 0.0f, 0.0f);
+	//skyTransformComp->Scale = Vector3(scale, scale, scale);
+	//
 
 
-	// 9. 메인 월드에 액터 추가.
-	MainWorld.AddEntity(actor);
-	MainWorld.AddEntity(cameraActor);
-	//MainWorld.AddEntity(skyActor);
+	//// Fbx Loader Test
+	//Character* fbxActor = new Character;
+	//auto fbxMeshComp = fbxActor->AddComponent<SkeletalMeshComponent>();
+	//if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Hulk_fbx/hulk_removeTwist.FBX"))
+	//{
+	//	//FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/charMob.FBX", fbxMeshComp);
+	//	FBXLoader::GetInstance()->GenerateSkeletalMeshFromFileData(L"../resource/FBX/Hulk_fbx/hulk_removeTwist.FBX", fbxMeshComp);
+	//}
+	//auto fbxAnimComp = fbxActor->AddComponent<AnimationComponent>();
+	//if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Hulk_fbx/Mutant_Punch_Retargeted.FBX"))
+	//{
+	//	FBXLoader::GetInstance()->GenerateAnimationFromFileData(L"../resource/FBX/Hulk_fbx/Mutant_Punch_Retargeted.FBX", fbxAnimComp);
+	//}
+	//auto fbxTransformComp = fbxActor->GetComponent<TransformComponent>();
+	//fbxTransformComp->Scale = Vector3(10.f, 10.f, 10.f);
+	//fbxTransformComp->Rotation = Vector3(0.0f, 90.0f, 0.0f);
+	//fbxTransformComp->Translation = Vector3(0.0f, 0.0f, 100.0f);
 
-	DirectionalLight* light = new DirectionalLight;
-	auto lightComp = light->GetComponent<DirectionalLightComponent>();
-	lightComp->Color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
-	lightComp->Direction = Vector4(1.0f, -1.0f, 1.0f, 1.0f);
-	MainWorld.AddEntity(light);
+	//auto fbxMovementComp = fbxActor->GetComponent<MovementComponent>();
+	//fbxMovementComp->Speed = 10.0f;
+	//fbxMovementComp->Destination = Vector3(0.0f, 0.0f, -100.0f);
+	//MainWorld.AddEntity(fbxActor);
 
-	DirectionalLight* light2 = new DirectionalLight;
-	auto lightComp2 = light2->GetComponent<DirectionalLightComponent>();
-	lightComp2->Color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
-	lightComp2->Direction = Vector4(-1.0f, 1.0f, 1.0f, 1.0f);
-	MainWorld.AddEntity(light2);
+	//Actor* landscapeActor = new Actor;
+	//auto landscape = landscapeActor->AddComponent<Landscape>();
+	//landscape->Build(8, 8, 7);
+	//landscape->SetCamera(DebugCamera);
+	//MainWorld.AddEntity(landscapeActor);
 
-	// 10. 카메라 시스템 및 랜더링 시스템 추가.
-	LightSystem* lightSystem = new LightSystem;
-	lightSystem->Initialize();
-	MainWorld.AddSystem(lightSystem);
-	MainWorld.AddSystem(new CameraSystem);
-	MainWorld.AddSystem(new RenderSystem);
-	MainWorld.AddSystem(new MovementSystem);
+	///*Actor* skySphereActor = new Actor;
+	//auto skySphereComp = skySphereActor->AddComponent<StaticMeshComponent>();
+	//if (FBXLoader::GetInstance()->Load(L"../resource/FBX/SkySphere.fbx"))
+	//{
+	//	FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/SkySphere.fbx", skySphereComp);
+	//}
+	//MainWorld.AddEntity(skySphereActor);
+	//auto skySphereTransform = skySphereActor->GetComponent<TransformComponent>();
+	//skySphereTransform->Scale = Vector3(20.0f, 20.0f, 20.0f);*/
 
-	// 추가
-	MainWorld.AddSystem(new UpdateAnimSystem);
 
-	//Effect Test
-	ParticleEffect* testEffect1 = new ParticleEffect(L"Fire");
-	ParticleEffect* testEffect2 = new ParticleEffect(L"Smoke");
-	ParticleEffect* testEffect3 = new ParticleEffect(L"Spark");
-	auto testEffectTransform1 = testEffect1->GetComponent<TransformComponent>();
-	auto testEffectTransform2 = testEffect2->GetComponent<TransformComponent>();
-	auto testEffectTransform3 = testEffect3->GetComponent<TransformComponent>();
-	testEffectTransform1->Translation = { 10.0f, 0.0f, 0.0f };
-	testEffectTransform2->Translation = { 20.0f, 0.0f, 0.0f };
-	testEffectTransform3->Translation = { 30.0f, 0.0f, 0.0f };
-	MainWorld.AddEntity(testEffect1);
-	MainWorld.AddEntity(testEffect2);
-	MainWorld.AddEntity(testEffect3);
+	///*Actor* skyDomeActor = new Actor;
+	//auto skyDomeComp = skyDomeActor->AddComponent<StaticMeshComponent>();
+	//if (FBXLoader::GetInstance()->Load(L"../resource/FBX/CloudDome.fbx"))
+	//{
+	//	FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/CloudDome.fbx", skyDomeComp);
+	//}
+	//Material* skyDomeMaterial = new Material;
+	//skyDomeMaterial->PixelShaderCodeName = L"T_UI";
+	//skyDomeMaterial->DiffuseTextureName = L"../resource/FBX/CloudSky2.png";
+	//MaterialManager::GetInstance()->AddMaterial(L"SkyDome", skyDomeMaterial);
+	//for (auto& mesh : skyDomeComp->Meshes)
+	//{
+	//	mesh.MaterialSlot = skyDomeMaterial;
+	//}
+	//MainWorld.AddEntity(skyDomeActor);
+	//auto skyDomeTransform = skyDomeActor->GetComponent<TransformComponent>();
+	//skyDomeTransform->Scale = Vector3(10.0f, 10.0f, 10.0f);*/
+
+	//Actor* skyDomeActor = new Actor;
+	//auto skyDomeComp = skyDomeActor->AddComponent<SkyDomeComponent>(); 
+	//skyDomeComp->Scale = Vector3(500.0f, 500.0f, 500.0f);
+	//MainWorld.AddEntity(skyDomeActor);
+	//MainWorld.AddSystem(new SkyRenderSystem);
+
+
+	//// 9. 메인 월드에 액터 추가.
+	//MainWorld.AddEntity(actor);
+	//MainWorld.AddEntity(cameraActor);
+	////MainWorld.AddEntity(skyActor);
+
+	//DirectionalLight* light = new DirectionalLight;
+	//auto lightComp = light->GetComponent<DirectionalLightComponent>();
+	//lightComp->Color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+	//lightComp->Direction = Vector4(1.0f, -1.0f, 1.0f, 1.0f);
+	//MainWorld.AddEntity(light);
+
+	//DirectionalLight* light2 = new DirectionalLight;
+	//auto lightComp2 = light2->GetComponent<DirectionalLightComponent>();
+	//lightComp2->Color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+	//lightComp2->Direction = Vector4(-1.0f, 1.0f, 1.0f, 1.0f);
+	//MainWorld.AddEntity(light2);
+
+	//// 10. 카메라 시스템 및 랜더링 시스템 추가.
+	//LightSystem* lightSystem = new LightSystem;
+	//lightSystem->Initialize();
+	//MainWorld.AddSystem(lightSystem);
+	//MainWorld.AddSystem(new CameraSystem);
+	//MainWorld.AddSystem(new RenderSystem);
+	//MainWorld.AddSystem(new MovementSystem);
+
+	//// 추가
+	//MainWorld.AddSystem(new UpdateAnimSystem);
+
+	////Effect Test
+	//ParticleEffect* testEffect1 = new ParticleEffect(L"Fire");
+	//ParticleEffect* testEffect2 = new ParticleEffect(L"Smoke");
+	//ParticleEffect* testEffect3 = new ParticleEffect(L"Spark");
+	//auto testEffectTransform1 = testEffect1->GetComponent<TransformComponent>();
+	//auto testEffectTransform2 = testEffect2->GetComponent<TransformComponent>();
+	//auto testEffectTransform3 = testEffect3->GetComponent<TransformComponent>();
+	//testEffectTransform1->Translation = { 10.0f, 0.0f, 0.0f };
+	//testEffectTransform2->Translation = { 20.0f, 0.0f, 0.0f };
+	//testEffectTransform3->Translation = { 30.0f, 0.0f, 0.0f };
+	//MainWorld.AddEntity(testEffect1);
+	//MainWorld.AddEntity(testEffect2);
+	//MainWorld.AddEntity(testEffect3);
 
 	return true;
 }
