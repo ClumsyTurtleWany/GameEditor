97e1be5ac7c53e9065afdc737079e5d4f17d3363
ClumsyTurtleWany <kdw41424142@gmail.com>
2023-03-23 15:24:23 +0900

    1. Camera Arm Component 추가. 2. 맵 리소스 추가. 3. 미 사용 리소스 정리.

--- SampleProject2/BattleScene.cpp
+++ SampleProject2/BattleScene.cpp
@@ -15,6 +15,7 @@
 #include "Character.h"
 #include "SelectAnimSystem.h"
 #include "BoundingBoxComponent.h"
+#include "CameraArmComponent.h"
 
 //Ãß°¡
 #include "ColliderSystem.h"
@@ -199,69 +200,69 @@ void BattleScene::Init_Map()
 	lightComp2->Direction = Vector4(-1.0f, 1.0f, 1.0f, 1.0f);
 	TheWorld.AddEntity(light2);
 
-	//for (int cnt = 0; cnt < 4; cnt++)
-	//{
-	//	Actor* backgroundBuliding = new Actor;
-	//	auto backgroundBulidingStaticMesh = backgroundBuliding->AddComponent<StaticMeshComponent>();
-	//	auto boundBox = backgroundBuliding->AddComponent<BoundingBoxComponent>(Vector3(1.2f, 0.5f, 1.0f));
-	//	if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Map/Warehouse/Warehouse.FBX"))
-	//	{
-	//		FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/Map/Warehouse/Warehouse.FBX", backgroundBulidingStaticMesh);
-	//	}
-	//	auto backgroundBulidingTransform = backgroundBuliding->GetComponent<TransformComponent>();
-	//	backgroundBulidingTransform->Scale = Vector3(100.0f, 100.0f, 100.0f);
-	//	backgroundBulidingTransform->Rotation = Vector3(0.0f, 0.0f, 0.0f);
-	//	backgroundBulidingTransform->Translation = Vector3(-500.0f + static_cast<float>(cnt) * 250, 40.0f, 300.0f);
-	//	TheWorld.AddEntity(backgroundBuliding);
-	//}
+	for (int cnt = 0; cnt < 4; cnt++)
+	{
+		Actor* backgroundBuliding = new Actor;
+		auto backgroundBulidingStaticMesh = backgroundBuliding->AddComponent<StaticMeshComponent>();
+		auto boundBox = backgroundBuliding->AddComponent<BoundingBoxComponent>(Vector3(1.2f, 0.5f, 1.0f));
+		if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Map/Warehouse/Warehouse.FBX"))
+		{
+			FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/Map/Warehouse/Warehouse.FBX", backgroundBulidingStaticMesh);
+		}
+		auto backgroundBulidingTransform = backgroundBuliding->GetComponent<TransformComponent>();
+		backgroundBulidingTransform->Scale = Vector3(100.0f, 100.0f, 100.0f);
+		backgroundBulidingTransform->Rotation = Vector3(0.0f, 0.0f, 0.0f);
+		backgroundBulidingTransform->Translation = Vector3(-500.0f + static_cast<float>(cnt) * 250, 40.0f, 300.0f);
+		TheWorld.AddEntity(backgroundBuliding);
+	}
 
-	//for (int cnt = 0; cnt < 4; cnt++)
-	//{
-	//	Actor* backgroundBuliding = new Actor;
-	//	auto backgroundBulidingStaticMesh = backgroundBuliding->AddComponent<StaticMeshComponent>();
-	//	auto boundBox = backgroundBuliding->AddComponent<BoundingBoxComponent>(Vector3(1.2f, 0.5f, 1.0f));
-	//	if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Map/Warehouse/Warehouse.FBX"))
-	//	{
-	//		FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/Map/Warehouse/Warehouse.FBX", backgroundBulidingStaticMesh);
-	//	}
-	//	auto backgroundBulidingTransform = backgroundBuliding->GetComponent<TransformComponent>();
-	//	backgroundBulidingTransform->Scale = Vector3(100.0f, 100.0f, 100.0f);
-	//	backgroundBulidingTransform->Rotation = Vector3(0.0f, 0.0f, 0.0f);
-	//	backgroundBulidingTransform->Translation = Vector3(-500.0f + static_cast<float>(cnt) * 250, 40.0f, -300.0f);
-	//	TheWorld.AddEntity(backgroundBuliding);
-	//}
+	for (int cnt = 0; cnt < 4; cnt++)
+	{
+		Actor* backgroundBuliding = new Actor;
+		auto backgroundBulidingStaticMesh = backgroundBuliding->AddComponent<StaticMeshComponent>();
+		auto boundBox = backgroundBuliding->AddComponent<BoundingBoxComponent>(Vector3(1.2f, 0.5f, 1.0f));
+		if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Map/Warehouse/Warehouse.FBX"))
+		{
+			FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/Map/Warehouse/Warehouse.FBX", backgroundBulidingStaticMesh);
+		}
+		auto backgroundBulidingTransform = backgroundBuliding->GetComponent<TransformComponent>();
+		backgroundBulidingTransform->Scale = Vector3(100.0f, 100.0f, 100.0f);
+		backgroundBulidingTransform->Rotation = Vector3(0.0f, 0.0f, 0.0f);
+		backgroundBulidingTransform->Translation = Vector3(-500.0f + static_cast<float>(cnt) * 250, 40.0f, -300.0f);
+		TheWorld.AddEntity(backgroundBuliding);
+	}
 
-	//for (int cnt = 0; cnt < 8; cnt++)
-	//{
-	//	Actor* container = new Actor;
-	//	auto staticMesh = container->AddComponent<StaticMeshComponent>();
-	//	auto boundBox = container->AddComponent<BoundingBoxComponent>(Vector3(150.0f, 250.0f, 300.0f));
-	//	if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Map/Container/Shipping_Container_A_-_Model.FBX"))
-	//	{
-	//		FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/Map/Container/Shipping_Container_A_-_Model.FBX", staticMesh);
-	//	}
-	//	auto transform = container->GetComponent<TransformComponent>();
-	//	transform->Scale = Vector3(0.2f, 0.2f, 0.2f);
-	//	transform->Rotation = Vector3(0.0f, 0.0f, 0.0f);
-	//	transform->Translation = Vector3(500.0f , 0.0f, -500.0f + static_cast<float>(cnt) * 125);
-	//	TheWorld.AddEntity(container);
-	//}
+	for (int cnt = 0; cnt < 8; cnt++)
+	{
+		Actor* container = new Actor;
+		auto staticMesh = container->AddComponent<StaticMeshComponent>();
+		auto boundBox = container->AddComponent<BoundingBoxComponent>(Vector3(150.0f, 250.0f, 300.0f));
+		if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Map/Container/Shipping_Container_A_-_Model.FBX"))
+		{
+			FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/Map/Container/Shipping_Container_A_-_Model.FBX", staticMesh);
+		}
+		auto transform = container->GetComponent<TransformComponent>();
+		transform->Scale = Vector3(0.2f, 0.2f, 0.2f);
+		transform->Rotation = Vector3(0.0f, 0.0f, 0.0f);
+		transform->Translation = Vector3(500.0f , 0.0f, -500.0f + static_cast<float>(cnt) * 125);
+		TheWorld.AddEntity(container);
+	}
 
-	//for (int cnt = 0; cnt < 8; cnt++)
-	//{
-	//	Actor* container = new Actor;
-	//	auto staticMesh = container->AddComponent<StaticMeshComponent>();
-	//	auto boundBox = container->AddComponent<BoundingBoxComponent>(Vector3(150.0f, 250.0f, 300.0f));
-	//	if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Map/Container/Shipping_Container_A_-_Model.FBX"))
-	//	{
-	//		FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/Map/Container/Shipping_Container_A_-_Model.FBX", staticMesh);
-	//	}
-	//	auto transform = container->GetComponent<TransformComponent>();
-	//	transform->Scale = Vector3(0.2f, 0.2f, 0.2f);
-	//	transform->Rotation = Vector3(0.0f, 0.0f, 0.0f);
-	//	transform->Translation = Vector3(500.0f, 0.0f, -500.0f + static_cast<float>(cnt) * 125);
-	//	TheWorld.AddEntity(container);
-	//}
+	/*for (int cnt = 0; cnt < 8; cnt++)
+	{
+		Actor* container = new Actor;
+		auto staticMesh = container->AddComponent<StaticMeshComponent>();
+		auto boundBox = container->AddComponent<BoundingBoxComponent>(Vector3(150.0f, 250.0f, 300.0f));
+		if (FBXLoader::GetInstance()->Load(L"../resource/FBX/Map/Container/Shipping_Container_C.FBX"))
+		{
+			FBXLoader::GetInstance()->GenerateStaticMeshFromFileData(L"../resource/FBX/Map/Container/Shipping_Container_C.FBX", staticMesh);
+		}
+		auto transform = container->GetComponent<TransformComponent>();
+		transform->Scale = Vector3(0.2f, 0.2f, 0.2f);
+		transform->Rotation = Vector3(0.0f, 0.0f, 0.0f);
+		transform->Translation = Vector3(510.0f, 40.0f, -500.0f + static_cast<float>(cnt) * 125);
+		TheWorld.AddEntity(container);
+	}*/
 
 	/*Actor* cargoShip = new Actor;
 	auto staticMesh = cargoShip->AddComponent<StaticMeshComponent>();
